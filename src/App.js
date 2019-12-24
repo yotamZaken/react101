d991e287cf52b6f74bbffffe4a4ef4930370af0c
yotamZaken <yotamzk@gmail.com>
2019-12-24 17:01:46 +0200

    End of section 5 - styling. Ran the eject script to enable css modules

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import uuidv4 from 'uuid/v4';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 
@@ -61,6 +61,7 @@ class App extends Component {
         };
 
         let persons = null;
+        let btnClass = '';
 
         if (this.state.showPersons) {
             persons = (
@@ -76,27 +77,23 @@ class App extends Component {
                 </div>
             );
 
-            // style.backgroundColor = 'red';
-            // style[':hover'] = {
-            //     backgroundColor: 'salmon',
-            //     color: 'black'
-            // }
+            btnClass = classes.Red;
         }
 
-        const classes = [];
+        const assignedClasses = [];
         if (this.state.persons.length <= 2) {
-            classes.push('red') // classes = ['red']
+            assignedClasses.push(classes.red) // assignedClasses = ['red']
         }
         if (this.state.persons.length <= 1) {
-            classes.push('bold') // classes = ['red', 'bold']
+            assignedClasses.push(classes.bold) // assignedClasses = ['red', 'bold']
         }
 
 
         return (
-                <div className="App">
+                <div className={classes.App}>
                     <h1>Hi, I'm a React App</h1>
-                    <p className={classes.join(' ')}>This is really working!</p>
-                    <button className="button" onClick={this.togglePersonsHandler}>
+                    <p className={assignedClasses.join(' ')}>This is really working!</p>
+                    <button className={btnClass} onClick={this.togglePersonsHandler}>
                         Toggle Persons
                     </button>
                     {persons}
