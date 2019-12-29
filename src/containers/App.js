3ba10b7964972f90f97308e9aafff3f9cd86cc7f
yotamZaken <yotamzk@gmail.com>
2019-12-29 18:01:18 +0200

    Lecture 88 - difference between class based and functional components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import uuidv4 from 'uuid/v4';
+
 import classes from './App.css';
-import Person from './Person/Person';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
     state = {
@@ -46,55 +48,23 @@ class App extends Component {
     };
 
     render() {
-        const style = {
-            backgroundColor: 'green',
-            color: 'white',
-            font: 'inherit',
-            border: '1px solid blue',
-            padding: '8px',
-            cursor: 'pointer',
-            ':hover': {
-                backgroundColor: 'lightgreen',
-                color: 'black'
-            }
-        };
-
         let persons = null;
-        let btnClass = '';
 
         if (this.state.showPersons) {
-            persons = (
-                <div>
-                    {this.state.persons.map((person, index) => {
-                        return <Person
-                            click={() => this.deletePersonHandler(index)}
-                            name={person.name}
-                            age={person.age}
-                            key={person.id}
-                            changed={(event) => this.nameChangedHandler(event, person.id)}/>
-                    })}
-                </div>
-            );
-
-            btnClass = classes.Red;
-        }
-
-        const assignedClasses = [];
-        if (this.state.persons.length <= 2) {
-            assignedClasses.push(classes.red) // assignedClasses = ['red']
-        }
-        if (this.state.persons.length <= 1) {
-            assignedClasses.push(classes.bold) // assignedClasses = ['red', 'bold']
+            persons = <Persons
+                persons={this.state.persons}
+                clicked={this.deletePersonHandler}
+                changed={this.nameChangedHandler}
+            />
         }
 
 
         return (
             <div className={classes.App}>
-                <h1>Hi, I'm a React App</h1>
-                <p className={assignedClasses.join(' ')}>This is really working!</p>
-                <button className={btnClass} onClick={this.togglePersonsHandler}>
-                    Toggle Persons
-                </button>
+                <Cockpit
+                    showPersons={this.state.showPersons}
+                    persons={this.state.persons}
+                    clicked={this.togglePersonsHandler}/>
                 {persons}
             </div>
         );
