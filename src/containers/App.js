44d7d8511c2dec24e443ff1ee0798caeb4081e03
yotamZaken <yotamzk@gmail.com>
2020-06-29 17:58:54 +0300

    Section 7 lecture 107 - higher order component intro

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,8 @@ import uuidv4 from 'uuid/v4';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import withClass from '../hoc/withClass';
+import Aux from '../hoc/Aux';
 
 class App extends Component {
     constructor(props) {
@@ -90,7 +92,7 @@ class App extends Component {
 
 
         return (
-            <div className={classes.App}>
+            <Aux>
                 <button onClick={() => {
                     this.setState({showCockpit: false})
                 }}>Remove Cockpit
@@ -102,10 +104,10 @@ class App extends Component {
                     clicked={this.togglePersonsHandler}
                 />) : null}
                 {persons}
-            </div>
+            </Aux>
         );
         // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
     }
 }
 
-export default App;
+export default withClass(App, classes.App);
