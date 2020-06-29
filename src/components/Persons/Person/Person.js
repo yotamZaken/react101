9587a2468a9cfbef10fa984a6281223bf7e0f99b
yotamZaken <yotamzk@gmail.com>
2020-06-29 18:34:50 +0300

    Using setState correctly

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 
 import Aux from '../../../hoc/Aux';
+import withClass from "../../../hoc/withClass";
 import classes from './Person.css';
 
 class Person extends Component {
@@ -20,4 +21,4 @@ class Person extends Component {
     }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
