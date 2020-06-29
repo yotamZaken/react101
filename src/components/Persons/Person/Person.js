867cbf3fed550fb1a745f3a3fc639bd40ece3bc1
yotamZaken <yotamzk@gmail.com>
2020-06-29 15:00:07 +0300

    section 7 lecture 103

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
+import Aux from '../../../hoc/Aux';
 import classes from './Person.css';
 
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <div className={classes.Person}>
+            <Aux>
                 <p onClick={this.props.click}> I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p> {this.props.children}</p>
                 <input
@@ -14,7 +15,7 @@ class Person extends Component {
                     onChange={this.props.changed}
                     value={this.props.name}
                 />
-            </div>
+            </Aux>
         )
     }
 }
